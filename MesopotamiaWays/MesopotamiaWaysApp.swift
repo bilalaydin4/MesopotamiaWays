091a6808705a21fa0a0b4eae2e0fae678d105625
bilalaydin4 <bilalaydibn414@gmail.com>
2025-11-24 02:35:03 +0300

    Initial Commit

---
+++ MesopotamiaWays/MesopotamiaWaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  MesopotamiaWaysApp.swift
+//  MesopotamiaWays
+//
+//  Created by Bilal AYDIN on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MesopotamiaWaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
